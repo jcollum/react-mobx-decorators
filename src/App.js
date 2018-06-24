7b9956a86e373ef6f0d12559fb8402354feb5e64
Justin Collum <jcollum@gmail.com>
2018-06-24 12:51:32 -0700

    added sound and computed value

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import { observable } from 'mobx';
+import { observable, computed } from 'mobx';
 import { observer } from 'mobx-react';
 import { Flex, Box } from 'grid-styled';
 import styled from 'styled-components';
+import Sound from 'react-sound';
+import soundFile from './public/smb.mp3'
 
 const Container = styled(Box)`
   max-width: 1024px;
 `;
+
 Container.defaultProps = {
   mx: 'auto'
 };
+
 const GreyBox = styled(Box)`
   background-color: #ccc;
   align-items: center;
@@ -27,6 +31,14 @@ const BlueFlex = styled(Flex)`
 @observer
 class App extends Component {
   @observable counter = 0;
+  @observable soundPlaying = null;
+
+  @computed
+  get playStatus() {
+    return this.soundPlaying === null
+      ? Sound.status.STOPPED
+      : this.soundPlaying === false ? Sound.status.PAUSED : Sound.status.PLAYING;
+  }
 
   onIncrement = () => {
     this.counter++;
@@ -34,6 +46,10 @@ class App extends Component {
 
   onDecrement = () => {
     this.counter--;
+  };
+
+  playSound = () => {
+    this.soundPlaying = !this.soundPlaying;
   };
 
   render() {
@@ -56,6 +72,22 @@ class App extends Component {
           <Box width={1 / 10}>{this.counter}</Box>
           <Box width={1}> </Box>
         </BlueFlex>
+
+        <Flex alignItems={'center'}>
+          <GreyBox width={1 / 2} px={2}>
+            <button onClick={this.playSound} type="button">
+              MARIO!
+            </button>
+            <Sound
+              url={soundFile}
+              playStatus={this.playStatus}
+              playFromPosition={0}
+              onLoading={this.handleSongLoading}
+              onPlaying={this.handleSongPlaying}
+              onFinishedPlaying={this.handleSongFinishedPlaying}
+            />
+          </GreyBox>
+        </Flex>
       </Container>
     );
   }
